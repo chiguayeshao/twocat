7e87e5f797c89c6e0a2028414786d2b50fb3af8e
Ethan Ye <chiguayeshao@gmail.com>
2024-11-20 18:34:46 +0800

    feat: 添加vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import { WalletContextProvider } from '@/components/providers/WalletProvider';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <WalletContextProvider>{children}</WalletContextProvider>
+        <Analytics />
         <Toaster />
       </body>
     </html>
