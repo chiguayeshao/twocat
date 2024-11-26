eabd5480079d1ec0d751cbbc076c5ec83e10c785
Ethan Ye <chiguayeshao@gmail.com>
2024-11-26 19:22:15 +0800

    feat: 调整header 加入搜索框

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -81,8 +81,6 @@ export function BaseLayout({ children, roomId }: BaseLayoutProps) {
 
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Header
-          room={room}
-          loading={loading}
           onTokenSelect={setSelectedTokenAddress}
         />
 
